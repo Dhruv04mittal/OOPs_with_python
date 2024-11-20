b086a463137ab00291918c14da561cdf5294921a
Dhruv04mittal <dhruvmittal1704@gmail.com>
2024-11-20 06:06:27 +0530

    Add files via upload

---
+++ Lab 1.py
@@ -0,0 +1,20 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> """ Q1 Print the following
+... 
+... a) print your first name
+... 
+... b) print your last name
+... 
+... c) print your complete name"""
+... # (a)
+... firstname = input("Enter your First name: ")
+... print(firstname)
+... #Output-Dhruv
+... # (b) 
+... lastname = input("Enter your Last name: ")
+... print(lastname)
+... #Output - Mittal
+... # (c)
+... fullname = f"{firstname} {lastname}"
+... print(fullname)
